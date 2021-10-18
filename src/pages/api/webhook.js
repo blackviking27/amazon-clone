aab9c11f7a1572c488bf5ced1ca319a648d2a687
Abhishek Prajapati <46470516+blackviking27@users.noreply.github.com>
2021-10-18 18:41:23 +0530

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -12,7 +12,7 @@ const app = !admin.apps.length
     : admin.app();
 
 // Connection to stripe
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
